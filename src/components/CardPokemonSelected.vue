5411df9ef1d5f6d25b7285c34fee5208772bbeea
Felipe Barros <filbarros@hotmail.com>
2024-09-07 09:34:50 -0300

    Projeto PokeVite VUE

---
+++ src/components/CardPokemonSelected.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+const pokemon = defineProps(["name", "xp", "height","img", "loading"])
+
+</script>
+
+<template>
+  <div class="card cardPokemonSelected"
+  :class="loading ? '' : 'animate__animated animate__flipInY'"
+  >
+
+    <img
+      :src="pokemon.img"
+      height="250"
+      class="card-img-top pt-2"
+       :alt="pokemon.name">
+
+    <div class="card-body">
+      <h5 class="card-title text-center">{{ pokemon.name }}</h5>
+      <hr>
+     <div class="row">
+      <section class="col">
+        <strong>XP:</strong>
+        <span>{{ pokemon.xp }}</span>
+      </section>
+      <section class="col">
+        <strong>Altura:</strong>
+        <span>{{ pokemon.height }}</span>
+      </section>
+     </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.cardPokemonSelected{
+  height: 450px;
+  background: rgb(31, 64, 230);
+  background: radial-gradient(circle, rgba(58, 68, 4, 0.5) 0%, rgba(0, 0, 0, 0.3) 100%);
+}
+
+</style>
---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,32 @@
+<script setup>
+const pokemon = defineProps(["name", "urlBaseSvg"])
+
+</script>
+
+
+<template>
+    <div class="col-4">
+        <div class="card p-2 mb-3 cardListPokemon">
+            <p class="text-center">{{ pokemon.name }}</p>
+            <img :src="urlBaseSvg" class="card-img-top" alt="..." height="80" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.cardListPokemon {
+    background: rgb(31, 64, 230);
+    background: radial-gradient(circle, rgba(46, 71, 196, 0.5) 0%, rgba(194, 31, 64, 0.3) 100%);
+    cursor: pointer;
+}
+
+.cardListPokemon :hover{
+    background: rgb(31, 64, 230);
+    background: radial-gradient(circle, rgba(31, 64, 230, 0.6) 0%, rgba(204, 14, 52, 0.4) 100%);
+    cursor: pointer;
+}
+
+.cardListPokemon img:hover{
+   padding: .1rem;
+}
+</style>
